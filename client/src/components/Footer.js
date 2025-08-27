0d328abc86d16cf38c871669f1008129e3796123
AshimaKamboj <ashimakamboj27@gmail.com>
2025-08-27 21:02:02 +0530

    changes in css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <div className="footer-container">
         
         {/* Left Section */}
-        <div className="footer-column">
+        <div className="footer-col">
           <h2 className="footer-logo">📅 Eventra</h2>
           <p>
             Discover, create, and book amazing events. <br />
